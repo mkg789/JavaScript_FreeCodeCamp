d7e45489afb102c7a1c25b76280a07090ae59cbc
Murali Krishna G <muralikrishnag2001@gmail.com>
2023-10-03 16:54:56 +0530

    Add files via upload
    Manipulating Complex Objects
    Sometimes you may want to store data in a flexible Data Structure. A JavaScript object is one way to handle flexible data. They allow for arbitrary combinations of strings, numbers, booleans, arrays, functions, and objects.
    
    Here's an example of a complex data structure:
    
    const ourMusic = [
      {
        "artist": "Daft Punk",
        "title": "Homework",
        "release_year": 1997,
        "formats": [ 
          "CD", 
          "Cassette", 
          "LP"
        ],
        "gold": true
      }
    ];
    This is an array which contains one object inside. The object has various pieces of metadata about an album. It also has a nested formats array. If you want to add more album records, you can do this by adding records to the top level array. Objects hold data in a property, which has a key-value format. In the example above, "artist": "Daft Punk" is a property that has a key of artist and a value of Daft Punk.
    
    Note: You will need to place a comma after every object in the array, unless it is the last object in the array.
    
    Add a new album to the myMusic array. Add artist and title strings, release_year number, and a formats array of strings.

---
+++ Manipulating Complex Objects.js
@@ -0,0 +1,24 @@
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    }
+  ];
+  myMusic[1]={
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    }
