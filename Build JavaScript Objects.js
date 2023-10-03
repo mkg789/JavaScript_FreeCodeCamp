d28bcdb1ea82e277497af64b41b120774359cfaa
Murali Krishna G <muralikrishnag2001@gmail.com>
2023-10-03 09:53:29 +0530

    Add files via upload
    Build JavaScript Objects
    You may have heard the term object before.
    
    Objects are similar to arrays, except that instead of using indexes to access and modify their data, you access the data in objects through what are called properties.
    
    Objects are useful for storing data in a structured way, and can represent real world objects, like a cat.
    
    Here's a sample cat object:
    
    const cat = {
      "name": "Whiskers",
      "legs": 4,
      "tails": 1,
      "enemies": ["Water", "Dogs"]
    };
    In this example, all the properties are stored as strings, such as name, legs, and tails. However, you can also use numbers as properties. You can even omit the quotes for single-word string properties, as follows:
    
    const anotherObject = {
      make: "Ford",
      5: "five",
      "model": "focus"
    };
    However, if your object has any non-string properties, JavaScript will automatically typecast them as strings.
    
    Make an object that represents a dog called myDog which contains the properties name (a string), legs, tails and friends.
    
    You can set these object properties to whatever values you want, as long as name is a string, legs and tails are numbers, and friends is an array.

---
+++ Build JavaScript Objects.js
@@ -0,0 +1,9 @@
+const myDog = {
+    // Only change code below this line
+    name : "mkg",
+    legs : 2,
+    tails : 0,
+    friends:["none","alone"]
+  
+    // Only change code above this line
+  };
