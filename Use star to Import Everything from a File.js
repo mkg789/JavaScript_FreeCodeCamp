359134aaa72513c227ff2213d674d7e60a71d351
Murali Krishna G <muralikrishnag2001@gmail.com>
2023-10-07 17:22:20 +0530

    Add files via upload
    Use * to Import Everything from a File
    Suppose you have a file and you wish to import all of its contents into the current file. This can be done with the import * as syntax. Here's an example where the contents of a file named math_functions.js are imported into a file in the same directory:
    
    import * as myMathModule from "./math_functions.js";
    The above import statement will create an object called myMathModule. This is just a variable name, you can name it anything. The object will contain all of the exports from math_functions.js in it, so you can access the functions like you would any other object property. Here's how you can use the add and subtract functions that were imported:
    
    myMathModule.add(2,3);
    myMathModule.subtract(5,3);
    The code in this file requires the contents of the file: string_functions.js, that is in the same directory as the current file. Use the import * as syntax to import everything from the file into an object called stringFunctions.

---
+++ Use star to Import Everything from a File.js
@@ -0,0 +1,6 @@
+// Use * to Import Everything from a File
+import * as stringFunctions from './string_functions.js'
+// Only change code above this line
+
+stringFunctions.uppercaseString("hello");
+stringFunctions.lowercaseString("WORLD!");
