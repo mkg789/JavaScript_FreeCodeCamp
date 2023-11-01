53b1b0fa36786ffaabb68742309172da55f21ac0
Murali Krishna G <muralikrishnag2001@gmail.com>
2023-11-01 13:20:55 +0530

    Add files via upload
    Convert HTML Entities
    Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities.

---
+++ Convert HTML Entities.js
@@ -0,0 +1,15 @@
+function convertHTML(str) {
+    const htmlEntities = {
+      "&": "&amp;",
+      "<": "&lt;",
+      ">": "&gt;",
+      '"': "&quot;",
+      "'": "&apos;"
+    };
+    
+    
+  
+    return str.replace(/([&<>\"'])/g,match=>htmlEntities[match]);
+  }
+  
+  console.log(convertHTML("Dolce & Gabbana"));
